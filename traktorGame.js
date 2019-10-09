b794d4689aea7c8907360802342092a7b7d0eb51
Patrick Trollip <ptrollip@gmail.com>
2019-10-09 21:27:04 +0100

    Second commit of JS file updates made

--- traktorGame.js
+++ traktorGame.js
@@ -1,20 +1,22 @@
 //Setting the canvas variable and getting it by the DOM getElement method using ID.
 var canvas = document.getElementById("myCanvas");
+
 //Setting the canvas to a 2d context.
 var ctx = canvas.getContext("2d");
+
 //setting the canvas width and height attributes.
-let screenWidth = 1000;
+let screenWidth = 1200;
 let screenHeight = 500;
-let width = 50;
+let width = 65;
 var isGameLive = true;
+
 //Game character prototype class.
 class GameCharacter{
-    constructor(x,y,width,height,color,speed){
+    constructor(x,y,width,height,speed){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.color = color;
         this.speed = speed;
         this.maxSpeed = 4;
     }
@@ -24,7 +26,7 @@ class GameCharacter{
     }
 
     moveVertically(){
-        if(this.y > screenHeight - 50 || this.y < 10)
+        if(this.y > screenHeight - 100 || this.y < 10)
         { this.speed = -this.speed;
         }
         
@@ -34,30 +36,31 @@ class GameCharacter{
     }
 }
 
-
+//The player's character.
+var Player = new GameCharacter(-50, 300, width, width,0)
 
 
 //Enemies.
 var enemies = [ 
 
 //First enemy.
-new GameCharacter(75, 50, width, width, "rgb(255, 26, 26)",2),
+new GameCharacter(75, 50, width, width,2),
 
 //Second enemy.
-new GameCharacter(220, 150, width, width, "rgb(255, 26, 26)",6),
+new GameCharacter(220, 150, width, width,6),
 
 //Third enemy.
-new GameCharacter(450, screenHeight - 100, width, width, "rgb(255, 26, 26)",5),
+new GameCharacter(450, screenHeight - 100, width, width,5),
 
 //Fourth enemy.
-new GameCharacter(600, 100, width, width, "rgb(255, 26, 26)",4),
+new GameCharacter(600, 100, width, width,4),
 
 //Fifth enemy
-new GameCharacter(800, screenHeight - 150, width, width, "rgb(255, 26, 26)",3),
+new GameCharacter(800, screenHeight - 150, width, width,3),
 ];
 
 //Goal.
-var Goal = new GameCharacter(screenWidth - 50, 250, width, width, "rgb(153, 0, 204)",0)
+var Goal = new GameCharacter(screenWidth -170, 300, width, width,0)
 
 //Sprites I made.
 var sprites = {};
@@ -65,69 +68,82 @@ var sprites = {};
 //Load sprites
 
 var loadSprites = function() {
+
+    //Background.
+    sprites.Background = new Image();
+    sprites.Background.src = "assets/Field photo.jpg";
+    
     //starting image (facing right)
-    sprites.player = new Image();
-    sprites.player.src = "assets/Tractor move right 1-1.png.png";
+    sprites.Player = new Image();
+    sprites.Player.src = "assets/Tractor move right 1-1.png.png";
 
     //next image (facing right)
-    sprites.player = new Image();
-    sprites.player.src = "assets/Tractor move right 1-2.png.png";
+    //sprites.player = new Image();
+    //sprites.player.src = "assets/Tractor move right 1-2.png.png";
 
     //next image (facing right)
-    sprites.player = new Image();
-    sprites.player.src = "assets/Tractor move right 1-3.png.png";
+    //sprites.player = new Image();
+    //sprites.player.src = "assets/Tractor move right 1-3.png.png";
 
     //next image (facing right)
-    sprites.player = new Image();
-    sprites.player.src = "assets/Tractor move right 1-4.png.png";
+    //sprites.player = new Image();
+    //sprites.player.src = "assets/Tractor move right 1-4.png.png";
 
     //last image if gameover (facing right)
-    sprites.player = new Image();
-    sprites.player.src = "assets/Tractor burn right 1-9.png.png";
+    //sprites.player = new Image();
+    //sprites.player.src = "assets/Tractor burn right 1-9.png.png";
+    
 
      //starting image (facing left)
-     sprites.player = new Image();
-     sprites.player.src = "assets/Tractor move left 1-5.png.png";
+     //sprites.player = new Image();
+     //sprites.player.src = "assets/Tractor move left 1-5.png.png";
  
      //next image (facing left)
-     sprites.player = new Image();
-     sprites.player.src = "assets/Tractor move left 1-6.png.png";
+     //sprites.player = new Image();
+     //sprites.player.src = "assets/Tractor move left 1-6.png.png";
  
      //next image (facing left)
-     sprites.player = new Image();
-     sprites.player.src = "assets/Tractor move left 1-7.png.png";
+     //sprites.player = new Image();
+     //sprites.player.src = "assets/Tractor move left 1-7.png.png";
  
      //next image (facing left)
-     sprites.player = new Image();
-     sprites.player.src = "assets/Tractor move left 1-8.png.png";
+     //sprites.player = new Image();
+     //sprites.player.src = "assets/Tractor move left 1-8.png.png";
 
      //last image if gameover (facing left)
-     sprites.player = new Image();
-     sprites.player.src = "assets/Tractor burn left 1-10.png.png";
+     //sprites.player = new Image();
+     //sprites.player.src = "assets/Tractor burn left 1-10.png.png";
 
      //Enemy sprite A
      sprites.enemies = new Image();
-     sprites.enemies.src = "assets/Enemy 2-1.png.png";
+    sprites.enemies.src = "assets/Enemy 2-1.png.png";
 
      //Enemy sprite B
-     sprites.enemies = new Image();
-     sprites.enemies.src = "assets/Enemy 2-2.png.png";
+     //sprites.enemies = new Image();
+     //sprites.enemies.src = "assets/Enemy 2-2.png.png";
 
      //Goal sprite 
-     sprites.goal = new Image();
-     sprites.goal.src = "assets/Goal-1.png.png";
+     sprites.Goal = new Image();
+     sprites.Goal.src = "assets/Goal-1.png.png";
 }
 
-//The player's character.
-var Player = new GameCharacter(10, 250, width, width, "rgb(0,0,255)",0)
+
 
 //PLayer controls
 
 //Player moves right
 document.onkeydown = function(event){
+    
     let keyPressed = event.keyCode;
+
     if (keyPressed == 39){
+
+
+        
+        
         Player.speed = Player.maxSpeed;
+
+      
     }
 
 //Player moves left
@@ -160,26 +176,30 @@ document.onkeyup = function(event) {
 
 //Draw function.
 var draw = function(){
-    ctx.clearRect(0, 0, screenWidth,
-         screenHeight);
+
+        // ctx.clearRect(0, 0, screenWidth,
+        // screenHeight);
+
+         //Draw Screen background 
+         ctx.beginPath();
+         ctx.fillStyle = "#ff6";
+         ctx.fillRect(0, 0, screenWidth, screenHeight);
+         //Draw background image.
+         ctx.drawImage(sprites.Background,0,0,canvas.width,canvas.height);
 
          //Draw player.
-         ctx.fillStyle = Player.color;
-         ctx.fillRect(Player.x, Player.y,
-            Player.width, Player.height);
-
+         ctx.drawImage(sprites.Player, Player.x, Player.y);
+         
+        //Draw goal.
+        ctx.drawImage(sprites.Goal, Goal.x, Goal.y);
 
         //Draw enemies.
-            enemies.forEach(function(element) {
-                ctx.fillStyle = element.color;
-         ctx.fillRect(element.x, element.y,
-            element.width, element.height);
+        enemies.forEach(function(element) {
+        ctx.drawImage(sprites.enemies, element.x, element.y,
+            );
             });
 
-            //Draw goal.
-            ctx.fillStyle = Goal.color;
-            ctx.fillRect(Goal.x, Goal.y,
-               Goal.width, Goal.height);
+            
 
 }
 
@@ -195,7 +215,7 @@ var update = function() {
 
     enemies.forEach(function(element) {
         if(checkCollisions(Player, element)){
-            endGameLogic("You lose!");
+            endGameLogic("You got abudcted!");
         }
       element.moveVertically (); 
      });
@@ -217,10 +237,13 @@ window.location = "";
 var step = function() {
     update();
     draw();
-if(isGameLive){
-    window.requestAnimationFrame(step);}
 
+if (isGameLive) {
+    window.requestAnimationFrame(step);
+}
     
-};
+}
+//Load sprites is called.
+loadSprites();
 //Step is called
 step();
