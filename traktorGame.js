bf56e6f6ace3c2939e0f73a7bb7085828999e984
Patrick Trollip <ptrollip@gmail.com>
2019-10-11 13:28:24 +0100

    Edited code and cleaned up as well as removed button

--- traktorGame.js
+++ traktorGame.js
@@ -1,68 +1,115 @@
 //Setting the canvas variable and getting it by the DOM getElement method using ID.
+
 var canvas = document.getElementById("myCanvas");
 
-//Setting the canvas to a 2d context.
+//Setting the canvas to a 2d context.-----------------------------------------------------
+
 var ctx = canvas.getContext("2d");
 
-//setting the canvas width and height attributes.
+//Setting the canvas width and height attributes.------------------------------------------
+
 let screenWidth = 1200;
 let screenHeight = 500;
 let width = 65;
 var isGameLive = true;
 
-//Audio
-var GameMusicOn = true;
+
+//Button to try increase difficulty---------------------------------------------------------------------------------
+/*
+let Button = document.querySelector("button");
+
+let number = this.speed;
+
+Button.addEventListener("click", () => {
+    number++;
+    
+}); */
+
+
+
+//Audio-------------------------------------------------------------------------------------
+
+//Sound for Background music.---------------------------------------------------------------
 
 BackgroundMusic = function()
+
 { document.getElementById("GameMusic");
     
 
     if(isGameLive = true) 
-    {GameMusic.play();
-    }
+
+    {GameMusic.play();}
                 
    else { 
             GameMusic.stop();
-            GameMusicOn = false;
         }
-    }
+}
+
+
+//Sound for  Aliens.----------------------------------------------------------------------------------
 
     Alien = function()
-{ document.getElementById("Ufo");
+{   document.getElementById("Ufo");
     
 
     if(isGameLive = true) 
-    {Ufo.play();
-    }
+
+       {Ufo.play();}
                 
    else { 
             Ufo.stop();
-            GameMusicOn = false;
         }
-    }
+}
+
+//Sound for tractor.------------------------------------------------------------------------------
 
     Tractor = function()
+{
+
+// THIS COMMENTED CODE MAKES THE GAME LAG AND DOES NOT CALL THE FARMER SOUND.
+    //  document.addEventListener("keydown",(e) =>
+    //{
+   //     if (e.which === 39 || e.which === 37) 
+    //    {    
+    //        document.getElementById("Farmer");
+    //        Tractor.play("Farmer");
+    //    }
+
+  //  });
+
+ //   document.addEventListener("keyup",(e) => 
+ //   {
+ //       if (e.which === 39 || e.which === 37)  
+ //       {
+ //           document.getElementById("Farmer");
+  //          Tractor.stop("Farmer");
+  //      }
+ //   });
+
+//THIS CODE CALLS THE FARMER SOUND BUT ON A LOOP.
+
     { document.getElementById("Farmer");
         
     
         if(isGameLive = true) 
-        {Farmer.play();
-        }
+
+        {Farmer.play();}
                     
        else { 
                 Farmer.stop();
-                GameMusicOn = false;
             }
-        }
-
+    }
+}
  
 
          
     
 
-//Game character prototype class.
-class GameCharacter{
-    constructor(x,y,width,height,speed){
+//Game character prototype class.-------------------------------------------------------------------
+class GameCharacter
+{
+    constructor(x, y, width, height, speed)
+    {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -70,58 +117,64 @@ class GameCharacter{
         this.speed = speed;
         this.maxSpeed = 4;
     }
-    moveHorizontally(){
+
+    moveHorizontally()
+    {
         this.x += this.speed;
-        
     }
 
-    moveVertically(){
+    moveVertically()
+    {
         if(this.y > screenHeight - 100 || this.y < 10)
-        { this.speed = -this.speed;
-        }
-        
-        
+
+        { this.speed = -this.speed;}
         
         this.y += this.speed;
     }
 }
 
-//The player's character.
-var Player = new GameCharacter(-50, 300, width, width,0)
+//The player's character.-----------------------------------------------------------------------------
+
+var Player = new GameCharacter(-50, 300, width, width, 0)
 
 
-//Enemies.
-var enemies = [ 
+//Enemies.--------------------------------------------------------------------------------------------
 
-//First enemy.
-new GameCharacter(75, 50, width, width,4),
+var enemies = 
+[ 
 
-//Second enemy.
-new GameCharacter(220, 150, width, width,7),
+    //First enemy.
+    new GameCharacter(75, 50, width, width, 4),
 
-//Third enemy.
-new GameCharacter(450, screenHeight - 100, width, width,9),
+    //Second enemy.
+    new GameCharacter(220, 150, width, width, 7),
 
-//Fourth enemy.
-new GameCharacter(600, 100, width, width,6),
+    //Third enemy.
+    new GameCharacter(450, screenHeight - 100, width, width, 9),
 
-//Fifth enemy.
-new GameCharacter(700, 100, width, width,5),
+    //Fourth enemy.
+    new GameCharacter(600, 100, width, width, 6),
 
-//Sixth enemy.
-new GameCharacter(850, screenHeight - 150, width, width,7),
+    //Fifth enemy.
+    new GameCharacter(700, 100, width, width, 5),
+
+    //Sixth enemy.
+    new GameCharacter(850, screenHeight - 150, width, width, 7),
 ];
 
 
-//Goal.
-var Goal = new GameCharacter(screenWidth -170, 300, width, width,0)
+//Goal.--------------------------------------------------------------------------------------------------------
 
-//Sprites I made.
+var Goal = new GameCharacter(screenWidth -170, 300, width, width, 0)
+
+//Sprites I made.------------------------------------------------------------------------------------------------
+
 var sprites = {};
 
-//Load sprites
+//Load sprites---------------------------------------------------------------------------------------------------
 
-var loadSprites = function() {
+var loadSprites = function () 
+{
 
     //Background.
     sprites.Background = new Image();
@@ -136,6 +189,11 @@ var loadSprites = function() {
      sprites.enemies = new Image();
     sprites.enemies.src = "assets/Enemy 2-1.png.png";
 
+    //Enemy sprite B
+
+    sprites.enemies = new Image();
+    sprites.enemies.src = "assets/Enemy 2-2.png.png";
+
 
      //Goal sprite 
      sprites.Goal = new Image();
@@ -144,44 +202,43 @@ var loadSprites = function() {
 
 
 
-//PLayer Moves movement
+//PLayer Moves movements-------------------------------------------------------------------------
 
 
 //Player moves right
 
-document.onkeydown = function(event){
+document.onkeydown = function(event)
+{
     
     let keyPressed = event.keyCode;
 
     if (keyPressed == 39)
     {
-        
         Player.speed = Player.maxSpeed;
-        
     }
 
  //Player moves left
-    else if(keyPressed == 37){
-        
+    else if(keyPressed == 37)
+    {
         Player.speed = -Player.maxSpeed;
-        
     }
     
 };
 ;
 
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) 
+{
     Player.speed = 0;
-    
 }
 
 //Collision functions.
-    var checkCollisions = function(rect1, rect2){
-        var xOverlap = Math.abs(rect1.x - rect2.x)
+    var checkCollisions = function (rect1, rect2)
+    {
+        var xOverlap = Math.abs (rect1.x - rect2.x)
         <= Math.max(rect1.width, rect2.width);
 
-        var yOverlap = Math.abs(rect1.y - rect2.y)
+        var yOverlap = Math.abs (rect1.y - rect2.y)
         <= Math.max(rect1.height, rect2.height);
 
         return xOverlap && yOverlap;
@@ -194,17 +251,19 @@ document.onkeyup = function(event) {
 
 
 //Draw function.
-var draw = function(){
+var draw = function()
+{
 
-        // ctx.clearRect(0, 0, screenWidth,
-        // screenHeight);
+         ctx.clearRect(0, 0, screenWidth, screenHeight);
 
          //Draw Screen background 
          ctx.beginPath();
+
          ctx.fillStyle = "#ff6";
+
          ctx.fillRect(0, 0, screenWidth, screenHeight);
          //Draw background image.
-         ctx.drawImage(sprites.Background,0,0,canvas.width,canvas.height);
+         ctx.drawImage(sprites.Background, 0, 0, canvas.width, canvas.height);
 
          //Draw player.
          ctx.drawImage(sprites.Player, Player.x, Player.y);
@@ -213,40 +272,45 @@ var draw = function(){
         ctx.drawImage(sprites.Goal, Goal.x, Goal.y);
 
         //Draw enemies.
-        enemies.forEach(function(element) {
-        ctx.drawImage(sprites.enemies, element.x, element.y,
-            );
-            });
-
-            
+        enemies.forEach(function(element) 
+        {
+            ctx.drawImage(sprites.enemies, element.x, element.y,);
+        });
 
 }
 
-//Update function to update the game.
+//Update function to update the game.-------------------------------------------------------------
 
-var update = function() {
+var update = function() 
+{
 
-    if(checkCollisions(Player, Goal)){
-    endGameLogic("You survived!");
+    if(checkCollisions(Player, Goal))
+    {
+        endGameLogic("You survived!");
 
     }
 
     Player.moveHorizontally();
 
-    enemies.forEach(function(element) {
-        if(checkCollisions(Player, element)){
+    enemies.forEach(function(element) 
+    {
+        if(checkCollisions(Player, element))
+        {
             endGameLogic("You got captured and probed by Aliens!");
+
         }
-      element.moveVertically (); 
-     });
-     enemies[0].moveVertically();
+     element.moveVertically (); 
+
+    });
+    
+    enemies[0].moveVertically();
      
 
      
          
       
 }
-//End Game message
+//End Game message--------------------------------------------------------------------------------
 
 var endGameLogic = function(text) 
 {
@@ -258,21 +322,19 @@ var endGameLogic = function(text)
 
 
 
-//Step function to put all the game events etc inside.
+//Step function to put all the game events etc inside.----------------------------------------------
+
 var step = function() 
 {
     update();
 
     draw();
     
-    
-
     if (isGameLive) 
     {
         window.requestAnimationFrame(step);
     
         
-
         function Audio()
         {
             BackgroundMusic();
@@ -287,7 +349,10 @@ var step = function()
     Audio();
     Tractor();
 }
-//Load sprites is called.
+//Load sprites is called.------------------------------------------------------------------
+
 loadSprites();
-//Step is called
+
+//Step is called--------------------------------------------------------------------------
+
 step();
